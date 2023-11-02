b3b01b103fd88bbe9d1b494ecc6d84490621d22d
Spydersk786 <khuranashivam92@gmail.com>
2023-11-02 15:33:20 +0530

    navbarallpage

--- schemas/galleryschema.js
+++ schemas/galleryschema.js
@@ -1,2 +1,8 @@
+const Joi = require('joi');
 
-
+module.exports.GallerySchema = Joi.object({
+    Galley: Joi.object({
+        image: Joi.string().required(),
+        description: Joi.string().required()
+    }).required()
+});
